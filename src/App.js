6ce721b480e5a1f8e9e21733d9accd36f01136e0
Allen Hu <assassincreedv@hotmail.com>
2024-03-21 10:16:07 -0500

    testing version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
+import SockJS from 'sockjs-client';
+import React, { useEffect, useState, useRef } from 'react';
+import { Client } from '@stomp/stompjs';
+
+
+
+
+const ConnectSoftphone = () => {
+  useEffect(() => {
+      // 确保这段代码在window.connect已加载后执行
+      const initCCP = () => {
+          const containerDiv = document.getElementById("softphone-container");
+          window.connect.core.initCCP(containerDiv, {
+              ccpUrl: "https://hitalentech-testing.my.connect.aws/connect/ccp-v2/", // 替换为你的CCP URL
+              loginPopupAutoClose: true, // 使用登录弹窗
+              softphone: {
+                  allowFramedSoftphone: true
+              }
+          });
+      };
+
+      if (window.connect) {
+          initCCP();
+      } else {
+          window.onload = initCCP;
+      }
+  }, []);
+
+  return <div id="softphone-container" style={{height: "600px" }} />;
+};
+
+const LiveCaptions = () => {
+  
+
+
+  const [captions, setCaptions] = useState([]);
+  const captionsContainerRef = useRef(null);
+
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: "ws://44.241.137.220:8082/ws", 
+      webSocketFactory: () => new SockJS("http://44.241.137.220:8082/ws"),
+      connectHeaders: {
+        login: "test",
+        passcode: "connect123",
+      },
+      debug: function (str) {
+        console.log('STOMP: ' + str);
+      },
+      onConnect: () => {
+        console.log("Connected");
+        client.subscribe('/topic/message', (message) => {
+          const body = JSON.parse(message.body);
+          console.log(body)
+          setCaptions(prevCaptions => [...prevCaptions, body]);
+        });
+      },
+      // 这里添加了其他配置项，如reconnectDelay
+    });
+
+    client.activate();
+
+    return () => {
+      if (client) {
+        client.deactivate();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (captionsContainerRef.current) {
+      const { scrollHeight, clientHeight } = captionsContainerRef.current;
+      captionsContainerRef.current.scrollTop = scrollHeight - clientHeight;
+    }
+  }, [captions]);
+
+  return (
+    <div ref={captionsContainerRef} className="captions-container">
+      {captions.map((caption, index) => (
+        <div key={index} className={`caption ${caption.speaker === 'Customer' ? 'customer' : 'agent'}`}>
+          <strong>{caption.speaker === 'Customer' ? '客户' : '服务代表'}:</strong> {caption.caption}
+        </div>
+      ))}
+    </div>
+  );
+
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{display:"flex", height:"600px"}}>
+        <div style={{width:"45%", marginRight:"10%"}}>
+          <p>test amaozn connect</p>
+          <ConnectSoftphone />
+        </div>
+        <div style={{height:"600px", width:"45%"}}>
+          <p>实时字幕：</p>
+          <LiveCaptions />
+        </div>
+      </div>
+     
+     
     </div>
   );
 }
